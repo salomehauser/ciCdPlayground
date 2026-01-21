9f47b32b4530571cd0afb127c2dd451001a0c4de
Salome Hauser <salome.hauser@tngtech.com>
2026-01-21 13:52:38 +0100

    personalize

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Salome Hauser",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Annoying stuff, little bugs"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Jenkins",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Understand whats happening in a pipline"],
   },
 });
