90294771572e878ceab98ab806879b438086982a
Salome Hauser <salome.hauser@tngtech.com>
2026-01-23 14:56:05 +0100

    undo fail test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -4,6 +4,6 @@ describe('The app', () => {
     it('tells that you are a butler', () => {
         cy.visit('')
         cy.get('h1')
-        .should('have.text', 'Hello, my name is Salome Fail!')
+        .should('have.text', 'Hello, my name is Salome Hauser!')
     })
   })
